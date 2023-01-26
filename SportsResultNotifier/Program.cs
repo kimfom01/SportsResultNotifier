e79d15ccc00c215c2c49a1a6394c9c0372f4a1cf
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-01-26 22:44:03 +0300

    Get the results and send email

--- SportsResultNotifier/Program.cs
+++ SportsResultNotifier/Program.cs
@@ -1,15 +1,31 @@
-﻿using SportsResultNotifier.Services;
+﻿using SportsResultNotifier.Models;
+using SportsResultNotifier.Services;
 
-Console.WriteLine("Hello, World!");
+var url = "https://www.basketball-reference.com/boxscores/";
 
+IScraperService scraper = new ScraperService(url);
 
-var scraper = new ScraperService("https://www.basketball-reference.com/boxscores/");
+var date = DateTime.Now.ToLongDateString();
+var title = $"NBA Games Played on {date}";
 
 var results = scraper.GetResults();
 
+string messageBody = "";
 foreach (var result in results)
 {
-    Console.WriteLine($"{result.HomeTeam.Name} - {result.HomeTeam.Score}");
-    Console.WriteLine($"{result.AwayTeam.Name} - {result.AwayTeam.Score}");
-    Console.WriteLine();
+    messageBody += $"{result.HomeTeam.Name} {result.HomeTeam.Score} : {result.AwayTeam.Score} {result.AwayTeam.Name}\n\n";
 }
+
+var message = new Message
+{
+    Title = title,
+    MessageBody = messageBody
+};
+
+var userName = "";
+var recipient = "";
+var password = "";
+
+IMailerService mailer = new MailerService(userName, password);
+
+mailer.SendEmail(message, recipient);
