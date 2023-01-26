829d3eac54542a472ac56e1be1b25e5c523cedef
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-01-26 03:05:44 +0300

    Call code from Program.cs

--- SportsResultNotifier/Program.cs
+++ SportsResultNotifier/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SportsResultNotifier.Services;
+
 Console.WriteLine("Hello, World!");
+
+
+var scraper = new ScraperService("https://www.basketball-reference.com/boxscores/");
+
+var results = scraper.GetResults();
+
+foreach (var result in results)
+{
+    Console.WriteLine($"{result.HomeTeam.Name} - {result.HomeTeam.Score}");
+    Console.WriteLine($"{result.AwayTeam.Name} - {result.AwayTeam.Score}");
+    Console.WriteLine();
+}
